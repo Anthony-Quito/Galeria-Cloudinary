0e8baa244cf8425d6bcf2abb31ef356f3af3cb40
Anthony Steven Quito Penadillo <anthony.quito@tecsup.edu.pe>
2021-10-22 22:51:19 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+const app = require('./app');
+
+app.listen(app.get('port'), () => {
+    console.log('App ejecutándose en puerto ', app.get('port'));
+    console.log('Enviroment: ', process.env.NODE_ENV);
+});
+
